d50af67e2679c73266bcb7655564fedf1c5fc92f
Daniil-PD <d576765765@gmail.com>
2023-11-19 12:48:22 +0400

    modified: chat

--- Miriteam_AEC_service/Miriteam_AEC_service/settings.py
+++ Miriteam_AEC_service/Miriteam_AEC_service/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'Miriteam_AEC_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'NAME': 'miri_base',
         'USER': 'postgres',
         'PASSWORD': '******',
